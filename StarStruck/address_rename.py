d8a04792877d00a0905b044ba067b550a8ec9170
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-06-09 06:42:37 -0700

    Update address_rename.py

--- StarStruck/address_rename.py
+++ StarStruck/address_rename.py
@@ -14,11 +14,13 @@ def rename_vmess_address(proxy, new_address):
         base64_str += '=' * (4 - missing_padding)
     try:
         decoded_str = base64.b64decode(base64_str).decode('utf-8')
+        print("Decoded VMess proxy JSON:", decoded_str)  # Debugging
         proxy_json = json.loads(decoded_str)
         proxy_json['add'] = new_address
         proxy_counter += 1
         encoded_str = base64.b64encode(json.dumps(proxy_json).encode('utf-8')).decode('utf-8')
         renamed_proxy = 'vmess://' + encoded_str
+        print("Renamed VMess proxy:", renamed_proxy)  # Debugging
         return renamed_proxy
     except Exception as e:
         print("Error processing vmess proxy: ", e)
@@ -35,6 +37,7 @@ def rename_vless_address(proxy, new_address):
         hostinfo = ':'.join(hostinfo_parts)
         renamed_proxy = userinfo + '@' + hostinfo
         proxy_counter += 1
+        print("Renamed VLess proxy:", renamed_proxy)  # Debugging
         return renamed_proxy
     except Exception as e:
         print("Error processing vless proxy: ", e)
